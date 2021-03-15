7b86f3c6673a1bd8bd17ed62459e9c1cb4171b7b
Martin Timothy Timko <45509732+mttcode@users.noreply.github.com>
2021-03-15 20:41:08 +0100

    Create Home.js

---
+++ ecommerce-front/src/core/Home.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import Layout from "./Layout";
+import { getProducts } from './Core';
+import Card from './Card';
+import "../styles.css";
+
+const Home = () => {
+
+    const [productsBySell, setProductsBySell] = useState([]);
+    const [productsByArrival, setProductsByArrival] = useState([]);
+    const [error, setError] = useState(false);
+
+
+    const loadProductsBySell = () => {
+        getProducts('sold').then(data => {
+            if (data.error) {
+                setError(data.error);
+            } else {
+                setProductsBySell(data);
+            }
+        });
+    };
+
+    const loadProductsByArrival = () => {
+        getProducts('createdAt').then(data => {
+            console.log(data);
+            if (data.error) {
+                setError(data.error);
+            } else {
+                setProductsByArrival(data);
+            }
+        });
+    };
+
+    useEffect(() => {
+        loadProductsByArrival();
+        loadProductsBySell();
+    }, []);
+
+
+
+    return (
+        <Layout
+            title="Travel Tours"
+            description="Travel agency application"
+            className="container-fluid"
+        >
+        
+        <h2 className="mb-4">Current tours</h2>
+            <div className="row">
+                {productsByArrival.map((product, i) => (
+                    <div key={i} className="col-4 mb-3">
+                        <Card product={product} />
+                    </div>
+                ))}
+            </div>
+
+            </Layout>
+    );
+
+};
+
+export default Home;
